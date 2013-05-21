1b76a577b35929af0a4ef0863b1a6f82d7d4be60
Thomas Blocher <blocher@in.tum.de>
2013-05-21 23:42:36 +0200

    Bug fix in src/scenarios/buildtest.sh

--- src/scenarios/buildtest.sh
+++ src/scenarios/buildtest.sh
@@ -1,3 +1,3 @@
 ncgen test.cdl -o test.nc
 cxxtestgen --error-printer SWE_TsunamiScenarioTest.hh -o runner.cpp
-g++ -o runner -I$CXXTEST SWE_Scenario.hh SWE_TsunamiScenario.hh runner.cpp -lnetcdf
+g++ -o runner runner.cpp -lnetcdf
