588d3feb1437621f1fc7549b73f560cfe9b34c16
Raphael Dümig <duemig@in.tum.de>
2013-05-25 20:33:45 +0200

    refactoring + handling of coastlines in SWE_TsunamiScenario
    1.) consistent naming for variables (*_bathy and *_displ)
    2.) *toGridCoordinates functions have been merged
    3.) water depths and landmass elevations below 20 meters are not allow anymore (see sheet of assignment)

--- src/scenarios/SWE_TsunamiScenario.hh
+++ src/scenarios/SWE_TsunamiScenario.hh
@@ -35,18 +35,22 @@
 
 using namespace std;
 
+typedef enum { BATHYMETRY, DISPLACEMENT } DataSource;
+
 
 class SWE_TsunamiScenario : public SWE_NetCDFScenario {
     
 public:
     
     /**
-     * load a scenario from a netCDF file
-     * 
-     * @param file the netCDF file to load
+     * minimum elevation of the landmass and minimum depth of the water
+     * This is needed for simulating the coastlines realistically.
      */
-    SWE_TsunamiScenario() {
-    };
+    const float bath_min_zero_offset;
+    
+    
+    SWE_TsunamiScenario() : bath_min_zero_offset(20.f)
+    {};
     
     // destructor
     ~SWE_TsunamiScenario() {
@@ -54,18 +58,46 @@ public:
         nc_close(ncid_displ);
     };
     
-    float getWaterHeight(float x, float y) { return -getPurBathymetry(x,y); };
+    
+    float getWaterHeight(float x, float y) {
+        float bath = getOriginalBathymetry(x,y);
+        
+        // apply the minimum height
+        if( bath > bath_min_zero_offset )
+            return bath_min_zero_offset;
+        else
+            return (-1) * bath;
+    };
     
     
     float getBathymetry(float x, float y) {
-        int err_val;
-        float result = 0.f;
+        int   err_val;
+        float displacement = 0.f;
+        float result;
+        
         size_t index[2];        
-        if(d_toGridCoordinates(x,y,&index[0], &index[1])){
-            if(err_val = nc_get_var1_float(ncid_displ, z_id_displ, index, &result))
-                cerr <<  nc_strerror(err_val) << endl;
+        
+        // try to get the indices for the displacement data
+        if( !toGridCoordinates(DISPLACEMENT, x ,y, &index[0], &index[1]) ) {
+            // we are outside the area where the displacement is relevant
+            result = getOriginalBathymetry(x,y);
         }
-        return result+getPurBathymetry(x,y);
+        else {
+            // get the displacement
+            err_val = nc_get_var1_float(ncid_displ, z_id_displ, index, &displacement);
+            if( err_val )
+                cerr << nc_strerror(err_val) << endl;
+            
+            result = getOriginalBathymetry(x,y) + displacement;
+        }
+        
+        // apply the minimum elevation or depth
+        if( result > (-1) * bath_min_zero_offset && result < 0)
+            result = (-1) * bath_min_zero_offset;
+        else if( result < bath_min_zero_offset && result >= 0)
+            result = bath_min_zero_offset;
+        
+        return result;
     };
     
     
@@ -76,31 +108,44 @@ public:
     
     // get boundary position TODO
     float getBoundaryPos(BoundaryEdge i_edge) {
+        //! error values from the netcdf calls
         int err_val;
+        
         float result;
+        
         size_t u0 = 0;
         size_t x = x_size_bathy-1;
         size_t y = y_size_bathy-1;
-        if( i_edge == BND_RIGHT ){
-            if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &x, &result))
+        
+        if( i_edge == BND_RIGHT ) {
+            err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &x, &result);
+            if( err_val )
                 cerr <<  nc_strerror(err_val) << endl;
-                result += (x_delta*0.5f);
-            }
-        else if( i_edge == BND_TOP ){
-            if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &y, &result))
+            
+            result += (x_delta_bathy*0.5f);
+        }
+        else if( i_edge == BND_TOP ) {
+            err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &y, &result);
+            if( err_val )
                 cerr <<  nc_strerror(err_val) << endl;
-                result += (y_delta*0.5f);
-                }
-        else if( i_edge == BND_BOTTOM ){
-            if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u0, &result))
+            
+            result += (y_delta_bathy*0.5f);
+        }
+        else if( i_edge == BND_BOTTOM ) {
+            err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u0, &result);  
+            if( err_val )
                 cerr <<  nc_strerror(err_val) << endl;
-                result -= (y_delta*0.5f);
-            }
-        else if( i_edge == BND_LEFT ){
-            if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u0, &result))
+            
+            result -= (y_delta_bathy*0.5f);
+        }
+        else if( i_edge == BND_LEFT ) {
+            err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u0, &result);
+            if( err_val )
                 cerr <<  nc_strerror(err_val) << endl;
-                result -= (x_delta*0.5f);
-            }
+            
+            result -= (x_delta_bathy*0.5f);
+        }
+        
         return result;
     };
     
@@ -119,37 +164,40 @@ public:
         int err_val;
         
         // number of dimensions of the variables 'x' and 'y'
-        int dim_x, dim_y, dim_x_displ, dim_y_displ;
+        int dim_x_bathy, dim_y_bathy, dim_x_displ, dim_y_displ;
         
         
-        // open .nc file
+        // open the bathymetry file
         if(err_val = nc_open(file_bathy, NC_NOWRITE, &ncid_bathy)) {
             cerr << nc_strerror(err_val) << endl;
             return err_val;
         }
+        
+        // open the displacements file
         if(err_val = nc_open(file_displ, NC_NOWRITE, &ncid_displ)) {
             cerr << nc_strerror(err_val) << endl;
             return err_val;
         }
+        
         // get the variables IDs 
-        if(err_val = nc_inq_varid(ncid_bathy, "x",  &x_id_bathy ))
+        if(err_val = nc_inq_varid(ncid_bathy, "x", &x_id_bathy ))
             cerr << nc_strerror(err_val) << endl;
-        if(err_val = nc_inq_varid(ncid_bathy, "y",  &y_id_bathy ))
+        if(err_val = nc_inq_varid(ncid_bathy, "y", &y_id_bathy ))
             cerr << nc_strerror(err_val) << endl;
-        if(err_val = nc_inq_varid(ncid_bathy, "z",  &z_id_bathy ))
+        if(err_val = nc_inq_varid(ncid_bathy, "z", &z_id_bathy ))
             cerr << nc_strerror(err_val) << endl;
         
-        if(err_val = nc_inq_varid(ncid_displ, "x",  &x_id_displ ))
+        if(err_val = nc_inq_varid(ncid_displ, "x", &x_id_displ ))
             cerr << nc_strerror(err_val) << endl;
-        if(err_val = nc_inq_varid(ncid_displ, "y",  &y_id_displ ))
+        if(err_val = nc_inq_varid(ncid_displ, "y", &y_id_displ ))
             cerr << nc_strerror(err_val) << endl;
-        if(err_val = nc_inq_varid(ncid_displ, "z",  &z_id_displ ))
+        if(err_val = nc_inq_varid(ncid_displ, "z", &z_id_displ ))
             cerr << nc_strerror(err_val) << endl;
         
         // get the number of dimensions contained by 'x' and 'y'
-        if(err_val = nc_inq_dimid(ncid_bathy, "x", &dim_x))
+        if(err_val = nc_inq_dimid(ncid_bathy, "x", &dim_x_bathy))
             cerr << nc_strerror(err_val) << endl;
-        if(err_val = nc_inq_dimid(ncid_bathy, "y", &dim_y))
+        if(err_val = nc_inq_dimid(ncid_bathy, "y", &dim_y_bathy))
             cerr << nc_strerror(err_val) << endl;
         
         if(err_val = nc_inq_dimid(ncid_displ, "x", &dim_x_displ))
@@ -158,41 +206,60 @@ public:
             cerr << nc_strerror(err_val) << endl;
         
         // get length x, y;
-        nc_inq_dimlen(ncid_bathy, dim_x, &x_size_bathy);
-        nc_inq_dimlen(ncid_bathy, dim_y, &y_size_bathy);
+        nc_inq_dimlen(ncid_bathy, dim_x_bathy, &x_size_bathy);
+        nc_inq_dimlen(ncid_bathy, dim_y_bathy, &y_size_bathy);
         
         nc_inq_dimlen(ncid_displ, dim_x_displ, &x_size_displ);
         nc_inq_dimlen(ncid_displ, dim_y_displ, &y_size_displ);
         
-        //get additional informations about the resulution of the x and y axes
+        
+        // get the distance of the first two values on the x- and y-axis and
+        // calculate the resolution of the axes from them
+        // (does only work for equidistant values in the grid)
         size_t u0 = 0;
         size_t u1 = 1;
-        if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u0, &y_start))
-            cerr <<  nc_strerror(err_val) << endl;
-        if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u1, &y_delta))
-            cerr <<  nc_strerror(err_val) << endl;
-        y_delta -= y_start;
-        if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u0, &x_start))
-            cerr <<  nc_strerror(err_val) << endl;
-        if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u1, &x_delta))
-            cerr <<  nc_strerror(err_val) << endl;
-        x_delta -= x_start;
         
-        if(err_val = nc_get_var1_float(ncid_displ, y_id_displ, &u0, &d_y_start))
+        float x_start_bathy, y_start_bathy;
+        float x_next_bathy,  y_next_bathy;
+        
+        float x_start_displ, y_start_displ;
+        float x_next_displ,  y_next_displ;
+        
+        // bathymetry file
+        if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u0, &y_start_bathy))
             cerr <<  nc_strerror(err_val) << endl;
-        if(err_val = nc_get_var1_float(ncid_displ, y_id_displ, &u1, &d_y_delta))
+        if(err_val = nc_get_var1_float(ncid_bathy, y_id_bathy, &u1, &y_next_bathy))
             cerr <<  nc_strerror(err_val) << endl;
-        d_y_delta -= d_y_start;
-        if(err_val = nc_get_var1_float(ncid_displ, x_id_displ, &u0, &d_x_start))
+        
+        if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u0, &x_start_bathy))
             cerr <<  nc_strerror(err_val) << endl;
-        if(err_val = nc_get_var1_float(ncid_displ, x_id_displ, &u1, &d_x_delta))
+        if(err_val = nc_get_var1_float(ncid_bathy, x_id_bathy, &u1, &x_next_bathy))
             cerr <<  nc_strerror(err_val) << endl;
-        d_x_delta -= d_x_start;
+        
+        y_delta_bathy = y_next_bathy - y_start_bathy;
+        x_delta_bathy = x_next_bathy - x_start_bathy;
+        
+        // displacements file
+        if(err_val = nc_get_var1_float(ncid_displ, y_id_displ, &u0, &y_start_displ))
+            cerr <<  nc_strerror(err_val) << endl;
+        if(err_val = nc_get_var1_float(ncid_displ, y_id_displ, &u1, &y_next_displ))
+            cerr <<  nc_strerror(err_val) << endl;
+        
+        if(err_val = nc_get_var1_float(ncid_displ, x_id_displ, &u0, &x_start_displ))
+            cerr <<  nc_strerror(err_val) << endl;
+        if(err_val = nc_get_var1_float(ncid_displ, x_id_displ, &u1, &x_next_displ))
+            cerr <<  nc_strerror(err_val) << endl;
+        
+        y_delta_displ = y_next_displ - y_start_displ;
+        x_delta_displ = x_next_displ - x_start_displ;
+        
+        
         return 0;
     }; 
     
 private:
-    // all variables are also declared for the displacement data and marked wit d_*    
+    // all variables for the bathymetric  data are marked with *_bathy
+    //                       displacement data are marked with *_displ
     
     // file id
     int ncid_bathy, ncid_displ;
@@ -205,46 +272,87 @@ private:
     size_t x_size_bathy, y_size_bathy;
     size_t x_size_displ, y_size_displ;
     
-    // float delta x[n] x[n+1];
-    float x_start, x_delta, y_start, y_delta, d_x_start, d_x_delta, d_y_start, d_y_delta;
+    // starting position and resolution of the bathymetry data
+    float x_start_bathy, y_start_bathy;
+    float x_delta_bathy, y_delta_bathy;
     
-    // This function makes a Fitting of the x an y coordinates to the grid of the basic Bathymetry Data 
-    void toGridCoordinates(float x_in, float y_in, size_t* x_out, size_t* y_out) {
-        *y_out = (size_t) (((y_in-y_start)/y_delta)+0.5f); 
-        *x_out = (size_t) (((x_in-x_start)/x_delta)+0.5f);
-        if(*x_out<0)
-            *x_out=0;
-        if(*y_out < 0)
-            *y_out = 0;
-        if(*x_out >= x_size_bathy)
-            *x_out = x_size_bathy-1;
-        if(*y_out >=y_size_bathy)
-            *y_out = y_size_bathy-1;
-        assert(*y_out <  y_size_bathy && *x_out < x_size_bathy);
-    };
+    // starting position and resolution of the displacement data
+    float x_start_displ, y_start_displ;
+    float x_delta_displ, y_delta_displ;
     
-    // This funktion checks if displacemant is relevant for the cordinates and if so it makes an fitting for them to the grid of the Displacement
-    // @return bool  ( displacement avaible for this position;
-    bool d_toGridCoordinates(float x_in, float y_in, size_t* x_out, size_t* y_out) {
-        if((x_in > d_x_start-(d_x_delta*0.5f)) && (x_in < d_x_start + (d_x_delta*(x_size_displ-0.5))) && 
-            (y_in > d_y_start-(d_y_delta*0.5f)) && (y_in < d_y_start + (d_y_delta*(y_size_displ-0.5)))){ 
-            *y_out = (size_t) (((y_in-d_y_start)/d_y_delta)+0.5f); 
-            *x_out = (size_t) (((x_in-d_x_start)/d_x_delta)+0.5f);
-            return true;
+    
+    /**
+     * toGridCoordinates calculates the indices for accessing the bathymetry
+     * and displacement data from the coordinates
+     * 
+     * @param source selects whether we want to get bathymetric or displacement data
+     * @param x_coord x-coordinate
+     * @param y_coord y-coordinate
+     * @param x_index place to write the index for the x-axis
+     * @param y_index place to write the index for the y-axis
+     * @return true if the coordinate is inside the boundaries of the data, false otherwise
+     */
+    bool toGridCoordinates(DataSource source,
+                           float x_coord, float y_coord,
+                           size_t* x_index, size_t* y_index) {
+        
+        float y_start, x_start;
+        float y_delta, x_delta;
+        float y_size,  x_size;
+        
+        // select the correct data  
+        if(source == BATHYMETRY) {
+            y_start = y_start_bathy;
+            x_start = x_start_bathy;
+            y_delta = y_delta_bathy;
+            x_delta = x_delta_bathy;
+            y_size  = y_size_bathy;
+            x_size  = x_size_bathy;
         }
-        else 
+        else {
+            y_start = y_start_displ;
+            x_start = x_start_displ;
+            y_delta = y_delta_displ;
+            x_delta = x_delta_displ;
+            y_size  = y_size_displ;
+            x_size  = x_size_displ;
+        }
+        
+        *y_index = (size_t) ( ( (y_coord - y_start) / y_delta ) + 0.5f ); 
+        *x_index = (size_t) ( ( (x_coord - x_start) / x_delta ) + 0.5f );
+        
+        // check if each of the indices is outside the boundaries
+        if(*x_index < 0 || *x_index >= x_size)
             return false;
+        if(*y_index < 0 || *y_index >= y_size)
+            return false;
+        
+        return true;
     };
     
-    //retuns Bathymetry data without displacement
-    float getPurBathymetry(float x, float y) {
+    
+    /**
+     * getOriginalBathymetry returns the bathymetry data without displacement
+     * 
+     * @param x x-coordinate
+     * @param y y-coordinate
+     * @return bathymetry at position (#x, #y) without displacement
+     */
+    float getOriginalBathymetry(float x, float y) {
         int err_val;
         size_t index[2];
+        
         float result;
-                
-        toGridCoordinates(x, y, &index[0], &index[1]);
-
-        if(err_val = nc_get_var1_float(ncid_bathy, z_id_bathy, index, &result))
+        
+        if(!toGridCoordinates(BATHYMETRY, x, y, &index[0], &index[1])) {
+            cerr << "Warning: bathymetry requested is outside the boundaries of the data!" << endl;
+            
+            // bathymetry outside of our data will be handled as landmass with minimum elevation
+            return bath_min_zero_offset;
+        }
+        
+        err_val = nc_get_var1_float(ncid_bathy, z_id_bathy, index, &result);
+        if( err_val )
             cerr <<  nc_strerror(err_val) << endl;
         
         return result;
