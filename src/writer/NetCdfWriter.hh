bf299a1a2fdebfd3c29d0ea1a272b21f813c48c4
Raphael Dümig <duemig@in.tum.de>
2013-05-25 13:12:38 +0200

    fixed whitespaces in NetCdfWriter.hh

--- src/writer/NetCdfWriter.hh
+++ src/writer/NetCdfWriter.hh
@@ -73,11 +73,11 @@ private:
 
   public:
     NetCdfWriter(const std::string &i_fileName,
-    			 const Float2D &i_b,
+                 const Float2D &i_b,
                  const BoundarySize &i_boundarySize,
                  int i_nX, int i_nY,
                  float i_dX, float i_dY, float ETime,
-		         bool newfile = false, 
+                 bool newfile = false, 
                  float i_originX = 0., float i_originY = 0., 
                  unsigned int i_flush = 0);
     virtual ~NetCdfWriter();
