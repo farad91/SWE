f144322b9a34bfd29357ac9236c3aebf500c23f2
Thomas Blocher <thomas@blocher.org>
2013-07-14 03:00:05 +0200

    j

--- src/blocks/SWE_DimensionalSplitting.cpp
+++ src/blocks/SWE_DimensionalSplitting.cpp
@@ -160,13 +160,13 @@ void SWE_DimensionalSplitting::computeNumericalFluxes()
 float SWE_DimensionalSplitting::computeHorizontalFluxes(){
     float maxEdgeSpeed = 0.f;
     #pragma omp parallel
-    float edgeSpeed = 0.f;
     float maxInnerEdgeSpeed = 0.f;
     float u[nx+2];
     #pragma omp for 
     for(int y=0; y<ny+2; y++){
         u[0] = hu[0][y] / h[0][y];
         for(int x=0; x<nx+1; x++){
+	 float edgeSpeed = 0.f;
         u[x+1] = hu[x+1][y] / h[x+1][y];
             fWaveSolver.computeNetUpdates(h[x][y],  h[x+1][y],
                                           hu[x][y], hu[x+1][y],
@@ -193,11 +193,11 @@ float SWE_DimensionalSplitting::computeVerticalFluxes(float dt){
     float maxEdgeSpeed = 0.f;
     #pragma omp parallel for schedule(dynamic,16)
     for(int x=1; x<nx+1; x++){
-        float edgeSpeed = 0.f;
         float maxInnerEdgeSpeed = 0.f;
         float u[ny+2];
         u[0] = hv[x][0] / h[x][0] ;
         for(int y=0; y<ny+1; y++){
+	    float edgeSpeed = 0.f;
             u[y+1] = hv[x][y+1] / h[x][y+1];
             fWaveSolver.computeNetUpdates(h[x][y] -(dt/dx * (hNetUpdatesRight[x-1][y] + hNetUpdatesLeft[x][y])),
                                           h[x][y+1] -(dt/dx*(hNetUpdatesRight[x-1][y+1] + hNetUpdatesLeft[x][y+1])),
@@ -223,13 +223,13 @@ float SWE_DimensionalSplitting::computeVerticalFluxes(float dt){
 float SWE_DimensionalSplitting::computeVerticalFluxes(){
     float maxEdgeSpeed = 0.f;
     #pragma omp parallel 
-    float edgeSpeed = 0.f;
     float maxInnerEdgeSpeed = 0.f;
     float u[ny+2];
     #pragma omp for schedule(dynamic,16)
     for(int x=1; x<nx+1; x++){
         u[0] = hv[x][0] / h[x][0];
         for(int y=0; y<ny+1; y++){
+	     float edgeSpeed = 0.f;
             u[y+1] = hv[x][y+1] / h[x][y+1];
             fWaveSolver.computeNetUpdates(h[x][y], h[x][y+1],
                                           hv[x][y], hv[x][y+1],
